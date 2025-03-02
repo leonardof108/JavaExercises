b0af91fdc234d844542257dd28914f6008e1824b
Leonardo Ferreira <leonardof108@gmail.com>
2025-03-01 23:04:14 -0300

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello, World!");
+        System.out.print("type something: ");
+        String message = scanner.nextLine();
+        System.out.printf("you typed %s", message);
+
     }
 }
